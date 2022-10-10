355c27b14c99332b1f6345078051efeba0ed6d5f
dthomas91 <davidthomas8891@gmail.com>
2022-10-09 19:03:07 -0700

    all but last javascript challenge complete

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,6 +14,7 @@
 
 // a) Create a test with expect statements using the variables provided.
 
+
 const secretCodeWord1 = "Lackadaisical"
 // Expected output: "L4ck4d41s1c4l"
 const secretCodeWord2 = "Gobbledygook"
@@ -21,11 +22,88 @@ const secretCodeWord2 = "Gobbledygook"
 const secretCodeWord3 = "Eccentric"
 // Expected output: "3cc3ntr1c"
 
+
+describe("codeMessage", () => {
+  it("returns a coded message", () => {
+    expect(codeMessage(secretCodeWord1)).toEqual("L4ck4d41s1c4l")
+    expect(codeMessage(secretCodeWord2)).toEqual("G0bbl3dyg00k")
+    expect(codeMessage(secretCodeWord3)).toEqual("3cc3ntr1c")
+  })
+})
+
+// ReferenceError: codeMessage is not defined
+
 // b) Create the function that makes the test pass.
+
+// input: String
+// output: String
+
+/* Pseudocode
+  1. create a function and pass in a string
+  2. split the string
+  3. map through the indexes 
+  4. use if/else statements to set the correct conditions
+  5. join the string back together
+*/
+
+
+const codeMessage = (str) => {
+  return str.split("").map((value) => {
+    if (value === "a" || value === "A") {
+      return "4" 
+    } else if (value === "e" || value === "E") {
+      return "3"
+    } else if (value === "i" || value === "I") {
+      return "1"
+    } else if (value === "o" || value === "O") {
+      return "0"
+    } else {
+      return value
+    }
+  }).join("")
+
+  // PASS  ./code-challenges.test.js
+
+  // ---------------------Code below this line was my first attempt at challenge 1--------------------
+
+// for (let i = 0; i < arr.length; i ++) {
+//   if (i === "a") {
+//    emptyArr.push("4")
+//   } else if (i === "e") {
+//     emptyArr.push("3")
+//   } else if (i === "i") {
+//    emptyArr.push("1")
+//   } else if (i === "o") {
+//    emptyArr.push("0")
+//   }
+// }
+// return emptyArr
+}
+
+// console.log(emptyArr)
+
+// const codeMessage = (str) => {
+//   return str.split("")
+// }
+// console.log(codeMessage(secretCodeWord1))
+
+// ---------------------------------------------------------------------------------------------------
+
+
+
 
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
 
 // a) Create a test with expects statement using the variable provided.
+
+describe("lessFruit", () => {
+  it("returns an array of all the words containing that particular letter", () => {
+    expect(lessFruit(fruitArray, letterA)).toEqual(["Mango", "Apricot", "Peach"])
+    expect(lessFruit(fruitArray, letterE)).toEqual(["Cherry", "Blueberry", "Peach"])
+  })
+})
+
+// ReferenceError: lessFruit is not defined
 
 const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
 
@@ -36,9 +114,50 @@ const letterE = "e"
 
 // b) Create the function that makes the test pass.
 
+/*
+  Pseudocode
+    1. create a function, pass in an array and a string
+    2. use the filter method on the array
+    3. bring the value of the array to lowercase to account for case sensitivity 
+    4. check if the array includes the string argument
+*/
+
+const lessFruit = (arr, str) => {
+    return arr.filter((value) => 
+    value.toLowerCase().includes(str.toLowerCase()) 
+   )
+}
+
+//  PASS  ./code-challenges.test.js
+
+/*--------------------- Code below this line was my first attempt at question 2 ---------------------*/
+
+// const lessFruit = (arr) => {
+//   return arr.map((value) => {
+//     if (value.include(str)) {
+//       return value
+//     }
+//   })
+// }
+//  console.log(lessFruit(letterA))
+
+// ----------------------------------------------------------------------------------------------
+
+
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
 
 // a) Create a test with expect statements using the variable provided.
+
+describe("fullHouse", () => {
+  it("determines whether or not the array is a full house", () => {
+    expect(fullHouse(hand1)).toEqual(true)
+    expect(fullHouse(hand2)).toEqual(false)
+    expect(fullHouse(hand3)).toEqual(false)
+    expect(fullHouse(hand4)).toEqual(true)
+  })
+})
+
+//  ReferenceError: fullHouse is not defined
 
 const hand1 = [5, 5, 5, 3, 3]
 // Expected output: true
@@ -50,3 +169,7 @@ const hand4 = [7, 2, 7, 2, 7]
 // Expected output: true
 
 // b) Create the function that makes the test pass.
+
+const fullHouse = (arr) => {
+  
+}
--- interview-questions.md
+++ interview-questions.md
@@ -8,44 +8,44 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is a gem?
 
-Your answer:
+Your answer: A gem in ruby is small portion of code that will be used often that creates functionality to avoid the developer having to retype code over and over.
 
-Researched answer:
+Researched answer: A gem is a snippet of code functionality. They will implement commonly used functions to an application. They also make it easier to share functionality. 
 
 2. What does the acronym CRUD stand for?
 
-Your answer:
+Your answer: CRUD stands for Create, Read, Update, and Delete. It refers to operations that are available to use in Rails
 
-Researched answer:
+Researched answer: Create, Read, Update, Delete. It refers to the four functions that are considered necessary to implement an application.
 
 3. What is Ruby on Rails?
 
-Your answer:
+Your answer: Ruby on rails is a development program that used the Ruby language of coding and puts it together with Rails methods.
 
-Researched answer:
+Researched answer: It is an open-source web application development framework written in the Ruby programming language. It provides a pre-built structure for development and everything you need to build a web app.
 
 4. What is a relational database? Are there other kinds of databases?
 
-Your answer:
+Your answer: A relational database is something that you can input data, and store it in different categories and you can update individual items in the database without having to access the entire code.
 
-Researched answer:
+Researched answer: It is a type of database that stores and provides access to data points that are related to one another. In a relational database, each row in the table is a record with a unique ID called the key. The columns of the table hold attributes of the data, and each record usually has a value for each attribute, making it easy to establish the relationships among data points.
 
 5. What are primary keys? Why are they important?
 
-Your answer:
+Your answer: The primary key is the way that data withing a database is categorized. For example a primary key for "name" would be where you would store all of the names in the database.
 
-Researched answer:
+Researched answer: Each row is has a primary key named "id" these are always unique. This is important so you can access the correct data even if the information in that row is similar to information in another row.
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. RESTful routes:
+1. RESTful routes: It maps from HTTP verbs to CRUD actions. It is used to create efficient, reusable routes.
 
-2. JSON:
+2. JSON: JSON stands for JavaScript Object Notation. It stores and transports data. It will be used when transferring data from the server to a webpage.
 
-3. ERB:
+3. ERB: erb stands for "Embedded RuBy". It will evaluate the Ruby to add content to the file dynamically, and will output a "pure" HTML file for rendering.
 
-4. Params:
+4. Params: Parameters allow us to add more information to our program by passing a value into the route rather than directly to the URL.
 
-5. API:
+5. API: Application Programming Interface transmits data in the form of JSON. The data stored is retrieved, created, updated, or deleted by controller methods. 
